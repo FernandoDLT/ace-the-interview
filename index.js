d93c751b7e1c09f6295439b683aa68b1ef9b7fbe
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-10-11 20:08:40 -0500

    PREP for palindrome function

---
+++ index.js
@@ -0,0 +1,25 @@
+// Function word say is a palindrome
+// word => str, no empty no !, no nums, no funny biz, no caps, one word
+// return true || false
+
+// example;
+// racecar => true
+// bob => bob
+// tacos => false
+
+// function isPalindrome(word) {
+    //word -> reverse that word - split reverse join
+    // let reversedWord = word.split('').reverse().join('')
+    //revWord === word
+//     if(word === reversedWord) {
+//         return true
+//     }else {
+//         return false
+//     }
+// }
+
+const isPalindrome = word => word === word.split('').reverse().join('')
+
+console.log(isPalindrome('racecar'), 'true')
+console.log(isPalindrome('bob'), 'true')
+console.log(isPalindrome('tacos'), 'false')
